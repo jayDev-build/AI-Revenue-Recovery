2651d72c03fecf51e9c8674e70a67738f7a764c7
Yashitjain <jainyashit11@gmail.com>
2026-08-23 20:26:46 +0530

    start

---
+++ src/main/java/AI/Revenue/Recovery/AI/Revenue/Recovery/AiRevenueRecoveryApplication.java
@@ -0,0 +1,13 @@
+package AI.Revenue.Recovery.AI.Revenue.Recovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRevenueRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRevenueRecoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AI/Revenue/Recovery/AI/Revenue/Recovery/AiRevenueRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package AI.Revenue.Recovery.AI.Revenue.Recovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRevenueRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
